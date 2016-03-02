346eecce2990ede242af9267075d0e0911e7a028
bit <bit@bit_pc>
2016-03-02 21:30:31 +0900

    0302Last

--- src/Exam04.java
+++ src/Exam04.java
@@ -12,6 +12,20 @@ public class Exam04 {
 		do{
 			System.out.println("수를 결정하였습니다. 맞추어 보세요\n"+MIN+"-"+MAX);
 			System.out.print(turn+">>"); myNum = sc.nextInt();
+			if(myNum > n){
+				System.out.println("더 낮게");
+				turn++;
+			}
+			else if(myNum < n){
+				System.out.println("더 높게");
+				turn++;
+			}
+			else{
+				System.out.println("맞았습니다");
+				System.out.println("다시하시겠습니까(y/n)");
+				String str = sc.nextLine();
+				if(str == "y") continueGame = 
+			}
 		}while(continueGame);
 	}
 }
